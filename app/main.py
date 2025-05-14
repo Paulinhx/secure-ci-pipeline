93d94a009746ce77a6dfa8a458a564876fe75b8d
Paulinhx <paul.desbats@gmail.com>
2025-05-14 18:42:08 +0200

    Update main.py with the eval insecure code

--- app/main.py
+++ app/main.py
@@ -1,10 +1,13 @@
 from flask import Flask
+
 app = Flask(__name__)
 
 @app.route('/')
 def home():
+    # 🔥 This line is insecure on purpose for testing Semgrep
+    eval("print('This is a Semgrep test finding')")  # Semgrep should flag this
     return "Secure CI/CD Example"
 
 if __name__ == "__main__":
     app.run(debug=True)
-# This is a simple Flask application that serves as a placeholder for the CI/CD pipeline.
+
